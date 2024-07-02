0cded2fc3682361590cab8b17b19824d4d914cdc
Muzaffar2807 <mmkhan2807@gmail.com>
2024-07-02 23:52:02 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
-  target: "serverless",
+  
 };
 
 module.exports = withMDX(nextConfig);
