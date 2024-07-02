3ece67741ccfd67a8c7c1c24f32eeac184bcce39
Muzaffar2807 <mmkhan2807@gmail.com>
2024-07-03 00:57:34 +0530

    asdf

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -107,7 +107,7 @@ export default function MobileMenu() {
                 className="flex rounded-lg px-2 py-1.5 text-gray-700 hover:bg-gray-100"
                 onClick={() => setMobileNavOpen(false)}
               >
-                Portfolio
+                Portfolio    
               </Link>
             </li>
             <li>
